965ae59dd84ad91d0299c29e327f79c033d84ec9
LulutasoAI <hinety3@gmail.com>
2021-11-07 12:56:46 +0900

    adding something I forgot to add from my laptop

---
+++ get_price_history.py
@@ -0,0 +1,25 @@
+from Portfolio_to_pie_chart import Portfolio_to_Piechart
+
+
+class get_history():
+    def __init__(self):
+        pass 
+    
+    def main(self):
+        pass 
+
+
+    def get_price_data(self):
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass 
